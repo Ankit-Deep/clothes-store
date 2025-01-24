a2cb7ac70de09e58d263e485ce9c6efa821c3991
Ankit Deep <AnkitDeep01@outlook.com>
2025-01-24 15:08:50 +0530

    Initial Deploy

--- src/pages/Customize.jsx
+++ src/pages/Customize.jsx
@@ -27,6 +27,8 @@ function Customize() {
     pocket: "No",
   });
 
+  
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({ ...prevData, [name]: value }));
