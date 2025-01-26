34c6cd5e6f59d70b06dfee8c705d20faf6b6ddfb
Ankit Deep <AnkitDeep01@outlook.com>
2025-01-26 16:02:15 +0530

    Solving deployment issue again on Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root : "./src",
-})
+  root: ".",
+  build: {
+    outDir: "dist", // Output directory for build files
+  },
+});
